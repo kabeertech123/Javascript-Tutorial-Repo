2643b46be44b80ba37167296a48c0ec9f926bea6
Kabeer Warsi <kwarsi541@gmail.com>
2026-05-21 21:45:21 +0100

    Creating rock paper scissors
    Just stuck on how to deal with user choice using for loop rather than using a bunch of even listeners

---
+++ 06-Bools-and-if-statements/script.js
@@ -0,0 +1,70 @@
+let userInput = "";
+let cuInput = 0;
+
+const rock = document.querySelector("#rock-btn");
+const paper = document.querySelector("#paper-btn");
+const scissors = document.querySelector("#scissors-btn");
+
+
+function handleChoice(choice){
+    
+    userInput = choice;
+    console.log(userInput);
+    
+}
+
+for(let i = 0; i < 3; i++){
+    
+}
+
+
+
+
+
+
+
+/* 
+
+rock.addEventListener("click", function playerInputRock(){
+
+    userInput = "rock";
+
+console.log(userInput);
+})
+
+paper.addEventListener("click", function playerInputPaper(){
+
+    userInput = "paper";
+
+console.log(userInput);
+})
+
+scissors.addEventListener("click", function playerInputScissors(){
+
+    userInput = "scissors";
+
+console.log(userInput);
+})
+
+
+
+
+
+Game loop
+
+Get user input and save in a var
+
+Generate a random number between 1-3
+
+if it is 1, then it is rock
+and so on
+
+compare userInput with CuInput
+
+Display the resutlt in h2
+
+Display the userInput and the Computer input
+
+Keep track of each win and lose 
+
+*/
